5b47f6bdfd89fefa8fc2592fcc0fa2039af75d2c
Song Xia <sx801@nyu.edu>
2024-06-13 12:33:04 -0400

    clean up and update readme

--- bash_scripts/local_select_conformer.bash
+++
@@ -1,5 +0,0 @@
-source /ext3/env.sh
-
-export PYTHONPATH=../dataProviders:$PYTHONPATH
-
-python select_conformer.py --pyg_name '$1' --save_folder $2 --pretrained_model $3
--- bash_scripts/local_train.bash
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH=../dataProviders:$PYTHONPATH
-
-python train.py --config_name $1
